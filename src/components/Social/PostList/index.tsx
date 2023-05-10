627a7c143c56a9697c81f7ffc0d847268f80a0c8
top350 <112688936+topAmity@users.noreply.github.com>
2023-05-11 01:00:05 +0700

    Add post ui

--- src/components/Social/PostList/index.tsx
+++ src/components/Social/PostList/index.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import type { UserInterface } from '../../../types/user.interface';
 import styles from './styles';
+import { SvgXml } from 'react-native-svg';
+import { likeXml } from '../../../svg/svg-xml-list';
 
 interface IPost {
   postId: string;
@@ -16,7 +18,22 @@ interface IPost {
 export default function PostList({}: IPost) {
   return (
     <View style={styles.postWrap}>
-      <Text>Post</Text>
+      <View style={styles.headerSection}>
+        <Text>Top Thanaphon</Text>
+      </View>
+      <View style={styles.bodySection}>
+        <Text style={styles.bodyText}>Post 555</Text>
+      </View>
+      <View style={styles.countSection}>
+        <Text style={styles.countText}>100 likes</Text>
+      </View>
+      <View style={styles.actionSection}>
+        <View style={styles.likeBtn}>
+        <SvgXml xml={likeXml} width="20" height="16" />
+        <Text style={styles.btnText}>Like</Text>
+        </View>
+      
+      </View>
     </View>
   );
 }
--- src/components/Social/PostList/styles.ts
+++ src/components/Social/PostList/styles.ts
@@ -1,12 +1,44 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   postWrap: {
     backgroundColor: '#FFFFFF',
     marginTop: 8,
-    height: 200
+    paddingHorizontal: 16,
   },
+  headerSection: {
+    paddingVertical: 8,
+  },
+  bodySection: {
+    paddingVertical: 8,
+    minHeight: 40,
+  },
+  countSection: {
+    paddingVertical: 8,
+  },
+  countText: {
+    fontSize: 13,
+    color: '#898E9E',
+  },
+  bodyText: {
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '400',
+  },
+  actionSection: {
+    paddingVertical: 11,
+    borderTopColor: '#EBECEF',
+    borderTopWidth: 1,
+  },
+  likeBtn: {
+    flexDirection: 'row',
+  },
+  btnText: {
+    color: '#898E9E',
+    fontSize: 15,
+    fontWeight: '600',
+    marginHorizontal: 4,
+  }
 });
 
 export default styles;
