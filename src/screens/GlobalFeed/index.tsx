3510efaf1f8d22e8ab92755a191a3dc671bf1cc0
Naing <naing@amity.co>
2024-01-25 18:01:31 +0700

    fix:ASC-00000 - Fix user feed rerender after profile update

--- src/screens/GlobalFeed/index.tsx
+++ src/screens/GlobalFeed/index.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 // import { useTranslation } from 'react-i18next';
 
@@ -14,24 +14,23 @@ import { getStyles } from './styles';
 import MyCommunity from '../../components/MyCommunity';
 
 import { amityPostsFormatter } from '../../util/postDataFormatter';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import globalFeedSlice from '../../redux/slices/globalfeedSlice';
-import { RootState } from '../../redux/store';
+import { RootState } from 'src/redux/store';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function GlobalFeed() {
+  const { postList } = useSelector((state: RootState) => state.globalFeed);
 
-  const { postList } = useSelector((state: RootState) => state.globalFeed)
-
-  const { updateGlobalFeed, deleteByPostId } = globalFeedSlice.actions
-  const dispatch = useDispatch() // ()=> dispatch(updateGlobalFeed())
+  const { updateGlobalFeed, deleteByPostId } = globalFeedSlice.actions;
+  const dispatch = useDispatch(); // ()=> dispatch(updateGlobalFeed())
 
   const styles = getStyles();
-  const { client, isConnected } = useAuth();
+  const { isConnected } = useAuth();
   const [postData, setPostData] = useState<IGlobalFeedRes>();
 
   const { data: posts = [], nextPage } = postData ?? {};
   const flatListRef = useRef(null);
-
 
   async function getGlobalFeedList(
     page: Amity.Page<number> = { after: 0, limit: 8 }
@@ -46,42 +45,47 @@ export default function GlobalFeed() {
       getGlobalFeedList(nextPage);
     }
   };
-  useEffect(() => {
-    if (isConnected) {
-      getGlobalFeedList();
-    }
-
-  }, [client]);
-  const getPostList = async () => {
+  useFocusEffect(
+    useCallback(() => {
+      if (isConnected) {
+        getGlobalFeedList();
+      }
+    }, [isConnected])
+  );
+  const getPostList = useCallback(async () => {
     if (posts.length > 0) {
-      const formattedPostList = await amityPostsFormatter(posts)
-      dispatch(updateGlobalFeed(formattedPostList))
+      const formattedPostList = await amityPostsFormatter(posts);
+      dispatch(updateGlobalFeed(formattedPostList));
     }
-  }
-
-  useEffect(() => {
-    getPostList();
-  }, [posts]);
+  }, [dispatch, posts, updateGlobalFeed]);
+  useFocusEffect(
+    useCallback(() => {
+      posts && getPostList();
+    }, [getPostList, posts])
+  );
 
   const onDeletePost = async (postId: string) => {
     const isDeleted = await deletePostById(postId);
     if (isDeleted) {
-      dispatch(deleteByPostId({ postId }))
+      dispatch(deleteByPostId({ postId }));
     }
   };
   const onPostChange = (post: IPost) => {
-    console.log('post:', post)
-
-  }
+    console.log('post:', post);
+  };
 
   return (
     <View style={styles.feedWrap}>
       <View style={styles.feedWrap}>
-
         <FlatList
           data={postList}
           renderItem={({ item, index }) => (
-            <PostList onDelete={onDeletePost} postDetail={item} onChange={onPostChange} postIndex={index} />
+            <PostList
+              onDelete={onDeletePost}
+              postDetail={item}
+              onChange={onPostChange}
+              postIndex={index}
+            />
           )}
           keyExtractor={(item) => item.postId.toString()}
           onEndReachedThreshold={0.5}
@@ -90,7 +94,6 @@ export default function GlobalFeed() {
           ListHeaderComponent={<MyCommunity />}
           extraData={postList}
         />
-
       </View>
     </View>
   );
