046b5d150af384ae3ecd37e085b6c549c8202934
Naing <naing@amity.co>
2024-03-28 14:08:24 +0700

    remove comment

--- src/v4/component/Social/ReplyCommentList/index.tsx
+++ src/v4/component/Social/ReplyCommentList/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from 'react';
-// import { useTranslation } from 'react-i18next';
 
 import {
   View,
