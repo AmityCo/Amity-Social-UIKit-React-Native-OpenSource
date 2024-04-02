5b0cfab22261c44a608098226276d0ac6514827c
Naing <naing@amity.co>
2024-04-02 18:04:00 +0700

    feat: uikit 4.0 dynamic config parse logic

--- src/providers/config-provider.tsx
+++ src/providers/config-provider.tsx
@@ -1,5 +1,9 @@
 import React, { createContext, ReactNode } from 'react';
-import { IConfigRaw, IUIKitConfig } from '~/v4/types/config.interface';
+import {
+  IConfigRaw,
+  IUIKitConfig,
+  IUIKitConfigOptions,
+} from '~/v4/types/config.interface';
 
 interface IConfigProviderProps {
   children: ReactNode;
@@ -10,20 +14,58 @@ export const ConfigContext = createContext<IUIKitConfig>({
   globalTheme: {},
   excludes: [],
   getConfig: () => ({}),
+  getUiKitConfig: () => ({}),
 });
 
 export const ConfigProvider = ({ children, configs }: IConfigProviderProps) => {
-  const configFile = configs;
   const getConfig = (id: string) => {
-    const value = configFile.customizations[id];
+    const value = configs.customizations[id];
     return value;
   };
 
-  const globalTheme = configFile.theme;
-  const excludes = configFile.excludes;
+  const getCorrectIDs = ({
+    page,
+    component,
+    element,
+  }: IUIKitConfigOptions): string[] => {
+    if (!component && !element) return [`${page}/*/*`];
+    if (!element) return [`${page}/${component}/*`, `*/${component}/*`];
+    return [
+      `${page}/${component}/${element}`,
+      `${page}/*/${element}`,
+      `*/${component}/${element}`,
+      `*/*/${element}`,
+    ];
+  };
+
+  const getCorrectConfig = (
+    correctIds: string[]
+  ): Record<string, string | string[] | Record<string, string>> => {
+    for (let i = 0; i < correctIds.length; i++) {
+      const correctId = correctIds[i];
+      const correctConfig = configs.customizations[correctId];
+      if (correctConfig) return correctConfig;
+    }
+  };
+
+  const getUiKitConfig = ({
+    page,
+    component,
+    element,
+  }: IUIKitConfigOptions) => {
+    const correctIds = getCorrectIDs({ page, component, element });
+    const correctConfig = getCorrectConfig(correctIds);
+    console.log(correctConfig);
+    return correctConfig;
+  };
+
+  const globalTheme = configs.theme;
+  const excludes = configs.excludes;
 
   return (
-    <ConfigContext.Provider value={{ globalTheme, excludes, getConfig }}>
+    <ConfigContext.Provider
+      value={{ globalTheme, excludes, getConfig, getUiKitConfig }}
+    >
       {children}
     </ConfigContext.Provider>
   );
