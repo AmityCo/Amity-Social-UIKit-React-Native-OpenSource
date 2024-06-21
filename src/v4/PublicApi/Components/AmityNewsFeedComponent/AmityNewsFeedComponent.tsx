0953e09ab1aaa00d9573a9db2317ea0b0d0fa9aa
ptchaya_p <ptchaya.p@gmail.com>
2024-06-21 17:35:12 +0700

    feat: remove floating button from newFeed page

--- src/v4/PublicApi/Components/AmityNewsFeedComponent/AmityNewsFeedComponent.tsx
+++ src/v4/PublicApi/Components/AmityNewsFeedComponent/AmityNewsFeedComponent.tsx
@@ -1,8 +1,4 @@
 import React, { FC, memo } from 'react';
-import FloatingButton from '../../../component/FloatingButton';
-import { useDispatch } from 'react-redux';
-import uiSlice from '../../../../redux/slices/uiSlice';
-import useAuth from '../../../../hooks/useAuth';
 import { View } from 'react-native';
 import AmityStoryTabComponent from '../AmityStoryTabComponent/AmityStoryTabComponent';
 import { AmityStoryTabComponentEnum } from '../../types';
@@ -17,19 +13,9 @@ type AmityNewsFeedComponentType = {
 const AmityNewsFeedComponent: FC<AmityNewsFeedComponentType> = ({
   pageId = PageID.WildCardPage,
 }) => {
-  const { client } = useAuth();
-  const dispatch = useDispatch();
   const { excludes } = useConfig();
   const componentId = ComponentID.newsfeed_component;
   const uiReference = `${pageId}/${componentId}/*`;
-  const { openPostTypeChoiceModal } = uiSlice.actions;
-  const openModal = () => {
-    dispatch(
-      openPostTypeChoiceModal({
-        userId: (client as Amity.Client).userId as string,
-      })
-    );
-  };
 
   if (excludes.includes(uiReference)) return null;
 
@@ -37,7 +23,6 @@ const AmityNewsFeedComponent: FC<AmityNewsFeedComponentType> = ({
     <View testID={uiReference} accessibilityLabel={uiReference}>
       <AmityStoryTabComponent type={AmityStoryTabComponentEnum.globalFeed} />
       <AmityGlobalFeedComponent pageId={pageId} />
-      <FloatingButton onPress={openModal} isGlobalFeed />
     </View>
   );
 };
