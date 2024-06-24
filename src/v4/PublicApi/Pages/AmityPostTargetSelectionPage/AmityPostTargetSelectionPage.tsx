a7e157d2064040c8fb9c2fb508a72f519d5f1913
ptchaya_p <ptchaya.p@gmail.com>
2024-06-24 11:19:03 +0700

    feat: add navigation to other page

--- src/v4/PublicApi/Pages/AmityPostTargetSelectionPage/AmityPostTargetSelectionPage.tsx
+++ src/v4/PublicApi/Pages/AmityPostTargetSelectionPage/AmityPostTargetSelectionPage.tsx
@@ -6,16 +6,63 @@ import PostTargetItem from './Components/PostTargetItem';
 import { Divider } from 'react-native-paper';
 import useAuth from '~/hooks/useAuth';
 
-const AmityPostTargetSelectionPage = () => {
+const AmityPostTargetSelectionPage = ({ route, navigation }) => {
   const { client } = useAuth();
   const user = useUser((client as Amity.Client).userId);
   const { communities, onNextCommunityPage } = useCommunities();
+  const { postType } = route.params;
+
+  const onSelectFeed = ({
+    targetId,
+    targetName,
+    targetType,
+    isPublic,
+    postSetting,
+    needApprovalOnPostCreation,
+  }: {
+    targetId: string;
+    targetName: string;
+    targetType: string;
+    isPublic?: boolean;
+    postSetting?: ValueOf<
+      Readonly<{
+        ONLY_ADMIN_CAN_POST: 'ONLY_ADMIN_CAN_POST';
+        ADMIN_REVIEW_POST_REQUIRED: 'ADMIN_REVIEW_POST_REQUIRED';
+        ANYONE_CAN_POST: 'ANYONE_CAN_POST';
+      }>
+    >;
+    needApprovalOnPostCreation?: string;
+  }) => {
+    const targetscreen = () => {
+      if (postType === 'post') return 'CreatePost';
+      if (postType === 'story') return 'CreateStory';
+      if (postType === 'poll') return 'CreatePoll';
+      if (postType === 'livestream') return 'CreateLivestream';
+      return null;
+    };
+
+    navigation.navigate(targetscreen(), {
+      targetId: targetId,
+      targetName: targetName,
+      targetType: targetType,
+      postSetting: postSetting,
+      isPublic: isPublic,
+      needApprovalOnPostCreation: needApprovalOnPostCreation,
+    });
+  };
+
   const renderItem = ({ item }: { item: Amity.Community }) => {
     return (
       <PostTargetItem
         key={item.communityId}
         displayName={item.displayName}
-        onSelect={() => {}}
+        onSelect={() =>
+          onSelectFeed({
+            targetId: item.communityId,
+            targetName: item.displayName,
+            targetType: 'community',
+          })
+        }
         avatarFileId={item.avatarFileId}
       />
     );
@@ -29,7 +76,13 @@ const AmityPostTargetSelectionPage = () => {
       </View>
       <PostTargetItem
         displayName="My Timeline"
-        onSelect={() => {}}
+        onSelect={() =>
+          onSelectFeed({
+            targetId: user.userId,
+            targetName: 'My Timeline',
+            targetType: 'user',
+          })
+        }
         avatarFileId={user.avatarFileId}
       />
       <Divider />
