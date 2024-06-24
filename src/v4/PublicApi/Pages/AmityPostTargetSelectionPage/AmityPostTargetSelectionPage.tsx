58ee451e6d4abdacacdc58a9a4de9ad85fc4fdce
ptchaya_p <ptchaya.p@gmail.com>
2024-06-24 11:01:27 +0700

    feat: add fetcing user info

--- src/v4/PublicApi/Pages/AmityPostTargetSelectionPage/AmityPostTargetSelectionPage.tsx
+++ src/v4/PublicApi/Pages/AmityPostTargetSelectionPage/AmityPostTargetSelectionPage.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
-import { useCommunities } from '~/v4/hook';
+import { useCommunities, useUser } from '~/v4/hook';
 import CloseButtonIconElement from '../../Elements/CloseButtonIconElement/CloseButtonIconElement';
 import PostTargetItem from './Components/PostTargetItem';
 import { Divider } from 'react-native-paper';
+import useAuth from '~/hooks/useAuth';
 
 const AmityPostTargetSelectionPage = () => {
-  // TODO: add logic to fetch community data
+  const { client } = useAuth();
+  const user = useUser((client as Amity.Client).userId);
   const { communities, onNextCommunityPage } = useCommunities();
-  const renderItem = ({ item }) => {
+  const renderItem = ({ item }: { item: Amity.Community }) => {
     return (
       <PostTargetItem
         key={item.communityId}
         displayName={item.displayName}
         onSelect={() => {}}
+        avatarFileId={item.avatarFileId}
       />
     );
   };
@@ -24,7 +27,11 @@ const AmityPostTargetSelectionPage = () => {
         <CloseButtonIconElement />
         <Text>Post to</Text>
       </View>
-      <PostTargetItem displayName="My Timeline" onSelect={() => {}} />
+      <PostTargetItem
+        displayName="My Timeline"
+        onSelect={() => {}}
+        avatarFileId={user.avatarFileId}
+      />
       <Divider />
       <Text>My Communities</Text>
       <FlatList
@@ -34,17 +41,6 @@ const AmityPostTargetSelectionPage = () => {
         onEndReachedThreshold={0.8}
         keyExtractor={(item) => item.communityId.toString()}
       />
-      <View>
-        {communities?.map((community) => {
-          return (
-            <PostTargetItem
-              key={community.communityId}
-              displayName={community.displayName}
-              onSelect={() => {}}
-            />
-          );
-        })}
-      </View>
     </View>
   );
 };
